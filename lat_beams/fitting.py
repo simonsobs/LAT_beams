4070e3c808812afc6654a6af454b7d315244d5de
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2025-03-21 00:29:17 +0000

    lm fitter

--- lat_beams/fitting.py
+++ lat_beams/fitting.py
@@ -8,17 +8,42 @@ from tqdm.auto import tqdm
 from scipy.optimize import minimize
 from scipy.stats import binned_statistic
 from scipy.ndimage import gaussian_filter1d
-from so3g.proj import coords, quat
+from so3g.proj import quat
 import sotodlib.coords.planets as planets
 from sotodlib import core
 import so3g
-from . import noise as nn
+from pqdm.processes import pqdm
+# from . import noise as nn
+
+def invsafe(matrix, thresh: float = 1e-14):
+    """
+    Safe SVD based psuedo-inversion of the matrix.
+    This zeros out modes that are too small when inverting.
+    Use with caution in cases where you really care about what the inverse is.
+    """
+    u, s, v = np.linalg.svd(matrix, False)
+    s_inv = np.array(np.where(np.abs(s) < thresh * np.max(s), 0, 1 / s))
+
+    return np.dot(np.transpose(v), np.dot(np.diag(s_inv), np.transpose(u)))
 
 
-# TODO get these working with our data
-def gauss_grad(x, y, x0, y0, amp, sigma):
+def invscale(matrix, thresh: float = 1e-14):
+    """
+    Invert and rescale a matrix by the diagonal.
+    This uses `invsafe` for the inversion.
+
+    """
+    diag = np.diag(matrix)
+    vec = np.array(np.where(diag != 0, 1.0 / np.sqrt(np.abs(diag)), 1e-10))
+    mm = np.outer(vec, vec)
+
+    return mm * invsafe(mm * matrix, thresh)
+
+# TODO: ellipticity
+def gauss_grad(x, y, x0, y0, amp, fwhm):
     dx = x - x0
     dy = y - y0
+    sigma = fwhm /(2 * np.sqrt(2 * np.log(2)))
     var = sigma**2
     gauss = amp*np.exp(-.5*(dx**2 + dy**2)/(var))
 
@@ -29,28 +54,6 @@ def gauss_grad(x, y, x0, y0, amp, sigma):
     grad = np.array([dfdx, dfdy, dady, dsdy])
 
     return gauss, grad
-
-def objective(aman, pars):
-    npar = len(pars)
-    chisq = np.array(0)
-    grad = np.zeros(npar)
-    curve = np.zeros((npar, npar))
-
-    pred_dat, grad_dat = gauss_grad(aman.xi, aman.eta, *pars)
-    
-    resid = aman.signal - pred_dat
-    resid_filt = nn.apply_noise(aman, resid) 
-    chisq = np.sum(resid * resid_filt)
-    grad_filt = np.zeros_like(grad_dat)
-    for i in range(npar):
-        grad_filt[i] =  nn.apply_noise(aman, grad_dat[i])
-    grad_filt = np.reshape(grad_filt, (npar, -1))
-    grad_dat = np.reshape(grad_dat, (npar, -1))
-    resid = resid.ravel()
-    grad = np.dot(grad_filt, np.transpose(resid)) 
-    curve = np.dot(grad_filt, np.transpose(grad_dat))
-
-    return chisq, grad, curve
 
 def get_xieta_src_centered_new( ctime, az, el, roll, sso_name,):
     """
@@ -99,7 +102,77 @@ def gaussian2d(xi, eta, a, xi0, eta0, fwhm_xi, fwhm_eta, phi):
     sim_data = a * np.exp(xi_coef + eta_coef)
     return sim_data
 
-def pointing_quickfit(aman, bandpass_range=(None, None), fwhm=np.deg2rad(0.5), max_rad=None, source='mars', bin_priors=False):
+def objective(aman, pars, filter_tod):
+    npar = len(pars)
+    chisq = np.array(0)
+    grad = np.zeros(npar)
+    curve = np.zeros((npar, npar))
+
+    pred_dat, grad_dat = gauss_grad(aman.xi, aman.eta, *pars) 
+    
+    aman.resid = aman.signal - pred_dat
+    aman = filter_tod(aman, signal_name='resid')
+    chisq = np.sum(aman.resid * aman.resid_filt)
+    grad_filt = np.zeros_like(grad_dat)
+    for i in range(npar):
+        aman.grad_buf[0] = grad_dat[i]
+        aman = filter_tod(aman, signal_name='grad_buf')
+        grad_filt[i] = aman.grad_buf_filt.copy().ravel() 
+    grad_filt = np.reshape(grad_filt, (npar, -1))
+    grad_dat = np.reshape(grad_dat, (npar, -1))
+    resid = aman.resid.ravel()
+    grad = np.dot(grad_filt, np.transpose(resid)) 
+    curve = np.dot(grad_filt, np.transpose(grad_dat))
+
+    return chisq, grad, curve
+
+def prior_pars(pars, priors):
+    prior_l, prior_u = priors
+    at_edge_l = pars <= prior_l
+    at_edge_u = pars >= prior_u
+    pars = np.where(at_edge_l, prior_l, pars)
+    pars = np.where(at_edge_u, prior_u, pars)
+
+    return pars
+
+def lm_fitter(aman, filter_tod, init_pars, bounds, max_iters=20, chitol=1e-5):
+    priors = np.array([[bound[i] for bound in bounds] for i in range(2)]) # Convert from scipy opt bounds to flat priors
+    pars = prior_pars(np.array(init_pars), priors)
+    chisq, grad, curve = objective(aman, pars, filter_tod)
+    errs = np.inf + np.zeros_like(pars)
+    delta_chisq = np.inf
+    lmd = 0
+    i = 0
+
+    for i in range(max_iters):
+        if delta_chisq < chitol:
+            break
+        curve_use = curve + (lmd * np.diag(np.diag(curve)))
+        # Get the step
+        step = np.dot(invscale(curve_use), grad)
+        new_pars = prior_pars(pars + step, priors) 
+        # Get errs
+        errs = np.sqrt(np.diag(invscale(curve_use)))
+        # Now lets get an updated model
+        new_chisq, new_grad, new_curve = objective(aman, new_pars, filter_tod)
+        new_delta_chisq = chisq - new_chisq
+
+        if new_delta_chisq > 0:
+            pars, chisq, grad, curve, delta_chisq = new_pars, new_chisq, new_grad, new_curve, new_delta_chisq
+            if lmd < .2:
+                lmd = 0
+            else:
+                lmd /= np.sqrt(2)
+        else:
+            if lmd == 0:
+                lmd = 1
+            else:
+                lmd *= 2
+
+    return pars, errs, i, delta_chisq
+
+
+def pointing_quickfit(aman, bandpass_range=(None, None), fwhm=np.deg2rad(0.5), max_rad=None, source='mars', bin_priors=False, lm_fit=False, show_tqdm=False):
     """
     Modified from analyze_bright_ptsrc
     """
@@ -120,6 +193,8 @@ def pointing_quickfit(aman, bandpass_range=(None, None), fwhm=np.deg2rad(0.5), m
     focal_plane.wrap("dist", np.zeros(len(aman.dets.vals), dtype=float), [(0, "dets")])
 
     xi, eta = get_xieta_src_centered_new(ts, az, el, roll, source)
+    aman.wrap("xi", xi, [(0, "samps")])
+    aman.wrap("eta", eta, [(0, "samps")])
 
     def filter_tod(am, signal_name='resid'):
         sig_filt_name = f"{signal_name}_filt"
@@ -136,19 +211,24 @@ def pointing_quickfit(aman, bandpass_range=(None, None), fwhm=np.deg2rad(0.5), m
             am[sig_filt_name] = fourier_filter(am, lowpass, **filt_kw)
         return am
 
-    def fit_func(x, fit_am, xi, eta):
+    def fit_func(x, fit_am):
         xi0, eta0, amp, fwhm = x
         model = gaussian2d(
-            xi, eta, amp, xi0, eta0, fwhm, fwhm, 0
+            fit_am.xi, fit_am.eta, amp, xi0, eta0, fwhm, fwhm, 0
         )
         fit_am.resid = (fit_am.signal.ravel() - model).reshape(fit_am.resid.shape)
         fit_am = filter_tod(fit_am, signal_name='resid')
         return np.sum(fit_am.resid * fit_am.resid_filt)
 
-    for i, det in enumerate(tqdm(aman.dets.vals)):
+    it = aman.dets.vals
+    if show_tqdm:
+        it = tqdm(aman.dets.vals) 
+    for i, det in enumerate(it):
         fit_am = aman.restrict("dets", [det], in_place=False)
         fit_am.wrap("resid", fit_am.signal.copy(), [(0, "dets"), (1, "samps")])
         fit_am.wrap("resid_filt", np.zeros_like(fit_am.signal), [(0, "dets"), (1, "samps")])
+        fit_am.wrap("grad_buf", np.zeros_like(fit_am.signal), [(0, "dets"), (1, "samps")])
+        fit_am.wrap("grad_buf_filt", np.zeros_like(fit_am.signal), [(0, "dets"), (1, "samps")])
         fit_am = filter_tod(fit_am) 
 
         max_idx = np.argmax(fit_am.resid_filt[0])
@@ -177,15 +257,30 @@ def pointing_quickfit(aman, bandpass_range=(None, None), fwhm=np.deg2rad(0.5), m
         sl = slice(int(np.percentile(msk_samps, 5)), int(np.percentile(msk_samps, 95)))
         fit_am.restrict("samps", sl)
 
-        res = minimize(fit_func, [xi0, eta0, amp, fwhm], bounds=[(xi0-max_rad, xi0+max_rad), (eta0-max_rad, eta0+max_rad), (-1, 10*amp), (fwhm/4, 4*fwhm)], args=(fit_am, xi[sl], eta[sl]), method="Nelder-Mead")
+        init_pars = [xi0, eta0, amp, fwhm]
+        bounds = [(xi0-max_rad, xi0+max_rad), (eta0-max_rad, eta0+max_rad), (-1, 10*amp), (fwhm/4, 4*fwhm)]
 
-        focal_plane.xi[i] = res.x[0]
-        focal_plane.eta[i] = res.x[1]
-        focal_plane.amp[i] = res.x[2]
-        focal_plane.fwhm[i] = res.x[3]
-        focal_plane.dist[i] = np.sqrt((res.x[0] - xi0)**2 + (res.x[1] - eta0)**2)
+        if not lm_fit:
+            res = minimize(fit_func, init_pars, bounds=bounds, args=(fit_am,), method="Nelder-Mead")
 
-        if not res.success:
-            focal_plane.amp[i] = -np.inf
+            focal_plane.xi[i] = res.x[0]
+            focal_plane.eta[i] = res.x[1]
+            focal_plane.amp[i] = res.x[2]
+            focal_plane.fwhm[i] = res.x[3]
+
+            if not res.success:
+                focal_plane.amp[i] = -np.inf
+
+        else:
+            res, errs, _, _= lm_fitter(fit_am, filter_tod, init_pars, bounds) 
+            focal_plane.xi[i] = res[0]
+            focal_plane.eta[i] = res[1]
+            focal_plane.amp[i] = res[2]
+            focal_plane.fwhm[i] = res[3]
+
+            # Lets say anything less than 3 sigma is a fluke
+            if res[2]/errs[2] < 3:
+                focal_plane.amp[i] = -np.inf
+        focal_plane.dist[i] = np.sqrt((focal_plane.xi[i] - xi0)**2 + (focal_plane.eta[i] - eta0)**2)
 
     return focal_plane 
