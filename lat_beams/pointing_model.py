3286ca37af8a430606a03ebb8751f701eb879981
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2025-05-08 17:49:58 +0000

    feat: 7 parameter model

--- lat_beams/pointing_model.py
+++ lat_beams/pointing_model.py
@@ -16,8 +16,19 @@ lat_params = {
     "eta_offset": -3.15087378e-01,
 }
 
+lat_params = {
+    "az_offset": 4.03704816e-08,
+    "el_offset": -1.39564042e-01,
+    "cr_offset": -1.75482402e-01,
+    "el_xi_offset": 1.40683504e-01,
+    "el_eta_offset": -3.15087378e-01,
+    "rx_xi_offset": 1.40683504e-01,
+    "rx_eta_offset": -3.15087378e-01,
+}
+
 
 def apply_pointing_model(params, az, el, roll):
+    cr = np.deg2rad(el - roll - np.deg2rad(60))
     _params = lat_params.copy()
     _params.update(params)
     params = _params
@@ -27,10 +38,12 @@ def apply_pointing_model(params, az, el, roll):
     q_enc = quat.rotation_lonlat(
         -1 * (az.copy() + params["az_offset"]), el.copy() + params["el_offset"]
     )
-    q_tel = quat.rotation_xieta(params["xi_offset"], params["eta_offset"])
-    q_roll = quat.euler(2, roll.copy() + params["el_offset"] - params["roll_offset"])
+    q_el_roll = quat.euler(2, el.copy() - np.deg2rad(60))
+    q_tel = quat.rotation_xieta(params["el_xi_offset"], params["el_eta_offset"])
+    q_cr_roll = quat.euler(2, -1*cr - params["cr_offset"])
+    q_rx = quat.rotation_xieta(params["rx_xi_offset"], params["rx_eta_offset"])
     new_az, el, roll = (
-        quat.decompose_lonlat(q_enc * q_tel * q_roll) * np.array([-1, 1, 1])[..., None]
+        quat.decompose_lonlat(q_enc * q_el_roll * q_tel * q_cr_roll * q_rx) * np.array([-1, 1, 1])[..., None]
     )
 
     # Stolen from elle
