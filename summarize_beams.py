d8342931b5da7228c3015999f421e5294a520667
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2025-05-15 19:36:53 +0000

    add script to make summary plots of beam fits

---
+++ summarize_beams.py
@@ -0,0 +1,224 @@
+# coding: utf-8
+import datetime as dt
+import os
+import h5py
+from sotodlib.core import AxisManager
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy.units as u
+
+nominal_fwhm = {"f090": 2, "f150": 1.3, "f220": 0.95, "f280": 0.83}  # arcmin
+fpath = "/so/home/saianeesh/data/beams/lat/source_map_fits/mars/beam_pars.h5"
+plot_dir = "/so/home/saianeesh/plots/beams/lat/source_map_fits/summary"
+os.makedirs(plot_dir, exist_ok=True)
+
+f = h5py.File(fpath, mode="r")
+obs_ids = []
+times = []
+stream_ids = []
+bands = []
+for o in f.keys():
+    for s in f[o].keys():
+        for b in f[o][s].keys():
+            obs_ids += [o]
+            times += [float(o.split("_")[1])]
+            stream_ids += [s]
+            bands += [b]
+
+limit_bands = ["f090", "f150"]
+msk = np.isin(bands, limit_bands)
+obs_ids = np.array(obs_ids)[msk]
+times = np.array(times)[msk]
+stream_ids = np.array(stream_ids)[msk]
+bands = np.array(bands)[msk]
+
+dates = [dt.date.fromtimestamp(ct) for ct in times]
+tdelt = (
+    np.array(
+        [
+            ct - dt.datetime(year=d.year, month=d.month, day=d.day).timestamp()
+            for ct, d in zip(times, dates)
+        ]
+    )
+    / 3600
+)
+
+amans = [
+    AxisManager.load(f[os.path.join(o, s, b)])
+    for o, s, b in zip(obs_ids, stream_ids, bands)
+]
+amp = u.Quantity([aman.amp for aman in amans])
+amp_err = u.Quantity([aman.amp_err for aman in amans])
+noise = u.Quantity([aman.noise for aman in amans])
+fwhm_x = u.Quantity([aman.fwhm_ra for aman in amans])
+fwhm_y = u.Quantity([aman.fwhm_dec for aman in amans])
+fwhm_data = u.Quantity([aman.data_fwhm for aman in amans])
+solid_angle = u.Quantity([aman.model_solid_angle for aman in amans])
+solid_angle_data = u.Quantity([aman.data_solid_angle for aman in amans])
+eps = np.abs(fwhm_x - fwhm_y) / (fwhm_x + fwhm_y)
+snr = amp / noise
+
+msk = amp / amp_err > 10
+msk *= snr > 20
+msk *= abs(1 - fwhm_x / fwhm_y) < 0.2
+msk *= abs(1 - fwhm_data / fwhm_x) < 0.2
+msk *= abs(1 - fwhm_data / fwhm_y) < 0.2
+msk *= solid_angle_data > 0
+
+for band in np.unique(bands[msk]):
+    bmsk = msk * (bands == band)
+    bmsk *= fwhm_data < 2 * nominal_fwhm[band] * 60 * u.arcsec
+    bmsk *= fwhm_data < np.percentile(fwhm_data[bmsk], 95)
+
+    plt.hist(fwhm_x[bmsk], alpha=0.5, bins=100, label="FWHM_x")
+    plt.hist(fwhm_y[bmsk], alpha=0.5, bins=100, label="FWHM_y")
+    plt.hist(fwhm_data[bmsk], alpha=0.5, bins=100, label="FWHM Data")
+    plt.legend()
+    plt.axvline(nominal_fwhm[band] * 60)
+    plt.title(f"FWHM at {band}")
+    plt.xlabel('FWHM (")')
+    plt.ylabel("Beam Maps")
+    plt.savefig(os.path.join(plot_dir, f"fwhm_{band}.png"))
+    plt.close()
+
+    plt.hist(eps[bmsk], bins=50)
+    plt.xlabel("Ellipticity")
+    plt.title(f"Ellipticity at {band}")
+    plt.savefig(os.path.join(plot_dir, f"ellipticity_{band}.png"))
+    plt.close()
+
+    plt.scatter(tdelt[bmsk], fwhm_x[bmsk], alpha=0.5, label="FWHM_x")
+    plt.scatter(tdelt[bmsk], fwhm_y[bmsk], alpha=0.5, label="FWHM_y")
+    plt.scatter(tdelt[bmsk], fwhm_data[bmsk], alpha=0.5, label="FWHM_data")
+    plt.legend()
+    plt.axhline(nominal_fwhm[band] * 60)
+    plt.xlabel("Time of day (hour UTC)")
+    plt.ylabel('FWHM (")')
+    plt.title(f"FWHM by Time at {band}")
+    plt.savefig(os.path.join(plot_dir, f"fwhm_time_{band}.png"))
+    plt.close()
+
+    plt.scatter(tdelt[bmsk], eps[bmsk])
+    plt.xlabel("Time of day (hour UTC)")
+    plt.ylabel("Ellipticity")
+    plt.title(f"Ellipticity by Time at {band}")
+    plt.savefig(os.path.join(plot_dir, f"ellipticity_time_{band}.png"))
+    plt.close()
+
+    plt.hist(solid_angle[bmsk], alpha=0.5, bins=100, label="Model")
+    plt.hist(solid_angle_data[bmsk], alpha=0.5, bins=100, label="Data")
+    plt.legend()
+    exp = 2 * np.pi * (np.deg2rad(nominal_fwhm[band] / 2.355 / 60) ** 2)
+    plt.xlim((exp / 3, 3 * exp))
+    plt.axvline(exp)
+    plt.title(f"Solid Angle at {band}")
+    plt.xlabel("Solid Angle (sr)")
+    plt.ylabel("Beam Maps")
+    plt.savefig(os.path.join(plot_dir, f"sang_{band}.png"))
+    plt.close()
+
+    plt.scatter(snr[bmsk], fwhm_x[bmsk], alpha=0.5, label="FWHM_x")
+    plt.scatter(snr[bmsk], fwhm_y[bmsk], alpha=0.5, label="FWHM_y")
+    plt.scatter(snr[bmsk], fwhm_data[bmsk], alpha=0.5, label="FWHM_data")
+    plt.legend()
+    plt.axhline(nominal_fwhm[band] * 60)
+    plt.xlabel("Naive SNR")
+    plt.ylabel('FWHM (")')
+    plt.title(f"FWHM by SNR at {band}")
+    plt.savefig(os.path.join(plot_dir, f"fwhm_snr_{band}.png"))
+    plt.close()
+
+
+meds = {
+    band: {
+        stream_id: [np.nan * u.arcsec, np.nan * u.arcsec, np.nan * u.arcsec]
+        for stream_id in np.unique(stream_ids[msk])
+    }
+    for band in np.unique(bands[msk])
+}
+for stream_id in np.unique(stream_ids[msk]):
+    smsk = msk * (stream_ids == stream_id)
+    for band in np.unique(bands[smsk]):
+        bmsk = smsk * (bands == band)
+        bmsk *= fwhm_data < 2 * nominal_fwhm[band] * 60 * u.arcsec
+        meds[band][stream_id] = [
+            np.median(fwhm_x[bmsk]),
+            np.median(fwhm_y[bmsk]),
+            np.median(fwhm_data[bmsk]),
+        ]
+
+        print(f"{stream_id} {band}: {np.sum(bmsk)} good maps")
+        plt.hist(fwhm_x[bmsk], alpha=0.5, bins=20, label="FWHM_x")
+        plt.hist(fwhm_y[bmsk], alpha=0.5, bins=20, label="FWHM_y")
+        plt.hist(fwhm_data[bmsk], alpha=0.5, bins=20, label="FWHM Data")
+        plt.xlim((0.5 * nominal_fwhm[band] * 60, 2 * nominal_fwhm[band] * 60))
+        plt.legend()
+        plt.axvline(nominal_fwhm[band] * 60)
+        plt.title(f"{stream_id} FWHM at {band}")
+        plt.xlabel('FWHM (")')
+        plt.ylabel("Beam Maps")
+        plt.savefig(os.path.join(plot_dir, f"fwhm_{stream_id}_{band}.png"))
+        plt.close()
+
+        plt.hist(eps[bmsk], bins=50)
+        plt.xlabel("Ellipticity")
+        plt.title(f"{stream_id} Ellipticity at {band}")
+        plt.savefig(os.path.join(plot_dir, f"ellipticity_{stream_id}_{band}.png"))
+        plt.close()
+
+        plt.scatter(tdelt[bmsk], fwhm_x[bmsk], alpha=0.5, label="FWHM_x")
+        plt.scatter(tdelt[bmsk], fwhm_y[bmsk], alpha=0.5, label="FWHM_y")
+        plt.scatter(tdelt[bmsk], fwhm_data[bmsk], alpha=0.5, label="FWHM_data")
+        plt.legend()
+        plt.axhline(nominal_fwhm[band] * 60)
+        plt.xlabel("Time of day (hour UTC)")
+        plt.ylabel('FWHM (")')
+        plt.title(f"{stream_id} FWHM by Time at {band}")
+        plt.savefig(os.path.join(plot_dir, f"fwhm_time_{stream_id}_{band}.png"))
+        plt.close()
+
+        plt.scatter(tdelt[bmsk], eps[bmsk])
+        plt.xlabel("Time of day (hour UTC)")
+        plt.ylabel("Ellipticity")
+        plt.title(f"Ellipticity by Time at {band}")
+        plt.savefig(os.path.join(plot_dir, f"ellipticity_time_{stream_id}_{band}.png"))
+        plt.close()
+
+        plt.hist(solid_angle[bmsk], alpha=0.5, bins=20, label="Model")
+        plt.hist(solid_angle_data[bmsk], alpha=0.5, bins=20, label="Data")
+        plt.legend()
+        exp = 2 * np.pi * (np.deg2rad(nominal_fwhm[band] / 2.355 / 60) ** 2)
+        plt.xlim((exp / 3, 3 * exp))
+        plt.axvline(exp)
+        plt.title(f"Solid Angle at {band}")
+        plt.xlabel("Solid Angle (sr)")
+        plt.ylabel("Beam Maps")
+        plt.savefig(os.path.join(plot_dir, f"sang_{stream_id}_{band}.png"))
+        plt.close()
+
+        plt.scatter(snr[bmsk], fwhm_x[bmsk], alpha=0.5, label="FWHM_x")
+        plt.scatter(snr[bmsk], fwhm_y[bmsk], alpha=0.5, label="FWHM_y")
+        plt.scatter(snr[bmsk], fwhm_data[bmsk], alpha=0.5, label="FWHM_data")
+        plt.legend()
+        plt.axhline(nominal_fwhm[band] * 60)
+        plt.xlabel("Naive SNR")
+        plt.ylabel('FWHM (")')
+        plt.title(f"{stream_id} FWHM by SNR at {band}")
+        plt.savefig(os.path.join(plot_dir, f"fwhm_snr_{stream_id}_{band}.png"))
+        plt.close()
+
+for band in meds.keys():
+    ufms = list(meds[band].keys())
+    x = u.Quantity([meds[band][ufm][0] for ufm in ufms])
+    y = u.Quantity([meds[band][ufm][1] for ufm in ufms])
+    d = u.Quantity([meds[band][ufm][2] for ufm in ufms])
+    plt.scatter(ufms, x, label="FWHM_x", alpha=0.5)
+    plt.scatter(ufms, y, label="FWHM_y", alpha=0.5)
+    plt.scatter(ufms, d, label="FWHM_data", alpha=0.5)
+    plt.legend()
+    plt.axhline(nominal_fwhm[band] * 60)
+    plt.xticks(rotation=60)
+    plt.title(f"Median FWHM at {band}")
+    plt.ylabel('FWHM (")')
+    plt.savefig(os.path.join(plot_dir, f"fwhm_ufm_{band}.png"))
+    plt.close()
