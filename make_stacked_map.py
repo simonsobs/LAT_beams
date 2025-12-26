c6fc46329d94f351e71f2f3e2104e175157d7fb8
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2025-12-25 19:17:34 -0800

    feat: full refactor of stacking code

---
+++ make_stacked_map.py
@@ -0,0 +1,248 @@
+import argparse
+import os
+
+import astropy.units as u
+import matplotlib.pyplot as plt
+import numpy as np
+import yaml
+from astropy import constants as const
+from sotodlib.core import Context
+from pixell import enmap
+
+from lat_beams import beam_utils as bu
+from lat_beams.utils import make_jobdb
+from lat_beams.plotting import plot_map
+
+
+def view_TQU(imap):
+    padded = imap
+    if len(imap) == 1:
+        padded = enmap.zeros((3,) + imap.shape[0:], imap.wcs)
+        padded[0][:] = imap[0][:]
+    return padded
+
+
+nominal_fwhm = {"f090": 2.0, "f150": 1.3, "f220": 0.95, "f280": 0.83}  # arcmin
+
+parser = argparse.ArgumentParser()
+parser.add_argument("cfg", help="Path to the config file")
+args = parser.parse_args()
+
+with open(args.cfg, "r") as f:
+    cfg = yaml.safe_load(f)
+
+# Get some global setting
+epochs = cfg.get("epochs", [(0, 2e10)])
+pointing_type = cfg.get("pointing_type", "pointing_model")
+nominal_fwhm = cfg.get("nominal_fwhm", nominal_fwhm)
+split_by = cfg.get(
+    "split_by", ["band", "tube_slot+band", "source+band", "source+tube_slot+band"]
+)
+extent = cfg["extent"] = cfg.get("extent", 900)
+mask_size = cfg.get("mask_size", 0.1)
+mask_size *= u.degree
+res = cfg["res"] = cfg.get("res", (10.0 / 3600.0) * np.pi / 180.0)
+pixsize = 3600 * np.rad2deg(res)
+log_thresh = cfg["log_thresh"] = cfg.get("log_thresh", 1e-3)
+ctx = Context(cfg.get("context", "/so/metadata/lat/contexts/smurf_detcal.yaml"))
+if ctx.obsdb is None:
+    raise ValueError("No obsdb in context!")
+op = np.ndarray.__iadd__
+
+# Setup folders
+root_dir = os.path.expanduser(cfg.get("root_dir", "~"))
+project_dir = cfg.get("project_dir", "beams/lat")
+plot_dir = os.path.join(
+    root_dir,
+    "plots",
+    project_dir,
+    f"{pointing_type}{(append!="")*'_'}{append}",
+    "stacks",
+)
+data_dir = os.path.join(
+    root_dir,
+    "data",
+    project_dir,
+    f"{pointing_type}{(append!="")*'_'}{append}",
+)
+os.makedirs(data_dir, exist_ok=True)
+fpath = os.path.join(data_dir, "beam_pars.h5")
+jdb = make_jobdb(None, data_dir)
+
+# Get jobs
+mjobdict = {
+    f"{job.tags['obs_id']}-{job.tags['wafer_slot']}-{job.tags['stream_id']}-{job.tags['band']}": job
+    for job in jdb.get_jobs(jclass="beam_map", jstate="done")
+}
+fjobdict = {
+    f"{job.tags['obs_id']}-{job.tags['wafer_slot']}-{job.tags['stream_id']}-{job.tags['band']}": job
+    for job in jdb.get_jobs(jclass="fit_map", jstate="done")
+}
+alljobstr = list(set(list(mjobdict.keys())) & set(list(fjobdict.keys())))
+mjobs = [mjobdict[jobstr] for jobstr in alljobstr]
+fjobs = [fjobdict[jobstr] for jobstr in alljobstr]
+
+# Load fits
+all_fits = bu.load_beam_fits_from_jobs(fpath, fjobs)
+
+# Make template map
+pix_extent = extent // pixsize
+twcs = enmap.wcsutils.build(
+    [0, 0], res=res, shape=(pix_extent, pix_extent), system="tan"
+)
+tmap = enmap.zeros((3, pix_extent, pix_extent), twcs)
+tweight = enmap.zeros((3, pix_extent, pix_extent), twcs)
+[[dec_min, ra_min], [dec_max, ra_max]] = 3600 * np.rad2deg(tmap.corners(corner=False))
+plt_extent = (ra_min, ra_max, dec_min, dec_max)
+
+
+# Loop through splits
+for split in split_by:
+    print(f"Splitting by {split}")
+    split_vec = bu.get_split_vec(all_fits, split, ctx)
+    for spl in np.unique(split_vec):
+        data_dir_spl = os.path.join(data_dir, "stacks", split, spl)
+        plot_dir_spl = os.path.join(plot_dir, split, spl)
+        os.makedirs(data_dir_spl, exist_ok=True)
+        os.makedirs(plot_dir_spl, exist_ok=True)
+
+        smsk = split_vec == spl
+        sfits = all_fits[smsk]
+        smjobs = mjobs[smsk]
+        sfjobs = fjobs[smsk]
+        fwhm_exp = np.array([nominal_fwhm[band] for band in sfits["band"]]) * u.arcmin
+        sang_exp = (2 * np.pi * (fwhm_exp.to(u.radian) / 2.355) ** 2).to(u.sr)
+        data_fwhm = bu.get_fit_vec(sfits, "data_fwhm")
+        solid_angle = bu.get_fit_vec(sfits, "data_solid_angle_corr")
+        msk = data_fwhm < 3 * fwhm_exp
+        msk *= data_fwhm < np.percentile(data_fwhm[msk], 95)
+        msk *= solid_angle < 3 * sang_exp
+        sfits = sfits[msk]
+        smjobs = smjobs[msk]
+        sfjobs = sfjobs[msk]
+        for epoch in epochs:
+            print(f"\tEpoch {epoch}")
+            times = sfits["time"]
+            tmsk = (times >= epoch[0]) * (times < epoch[1])
+            if np.sum(tmsk) == 0:
+                print(f"\t\tNo maps found! Skipping...")
+                continue
+            mcoadd = enmap.zeros_like(tmap)
+            wcoadd = enmap.zeros_like(tweight)
+            rmcoadd = enmap.zeros_like(tmap)
+            rwcoadd = enmap.zeros_like(tweight)
+            for fit, mjob, fjob in zip(sfits[tmsk], smjobs[tmsk], sfjobs[tmsk]):
+                # Load
+                try:
+                    solved = enmap.read_map(os.path.join(data_dir, mjob.tags["solved"]))
+                    weights = enmap.read_map(
+                        os.path.join(data_dir, mjob.tags["weights"])
+                    )[np.diag_indices(len(solved))]
+                    resid = enmap.read_map(os.path.join(data_dir, fjob.tags["resid"]))
+                    if len(resid.shape) == 2:
+                        resid = resid.reshape((1,) + resid.shape)
+                    resid_weights = enmap.read_map(
+                        os.path.join(data_dir, fjob.tags["resid_weights"])
+                    )
+                    if len(resid_weights.shape) == 4:
+                        resid_weights = resid_weights[
+                            np.diag_indices(len(resid_weights))
+                        ]
+                    resid_weights = resid_weights.reshape(resid.shape)
+                except FileNotFoundError:
+                    print(f"Maps missing for job: {mjob}")
+                    continue
+
+                # Make everything look like TQU
+                solved = view_TQU(solved)
+                weights = view_TQU(weights)
+                resid = view_TQU(resid)
+                resid_weights = view_TQU(resid_weights)
+                if not np.all(
+                    np.array(
+                        [len(solved), len(weights), len(resid), len(resid_weights)]
+                    )
+                    == 3
+                ):
+                    raise ValueError("Maps don't look like TQU!")
+
+                # Crop, recenter, and normalize
+                cent = np.array(
+                    (fit["aman"].eta0.to(u.rad).value, fit["aman"].xi0.to(u.rad).value)
+                )
+                solved = enmap.reproject.thumbnails(
+                    solved,
+                    coords=cent,
+                    oshape=((len(solved),) + (pix_extent, pix_extent)),
+                    owcs=twcs,
+                )
+                solved = (
+                    solved - np.array([fit["aman"].off.value, 0, 0]).reshape((3, 1, 1))
+                ) / fit["aman"].amp.value
+                weights = enmap.reproject.thumbnails_ivar(
+                    weights,
+                    coords=cent,
+                    oshape=((len(solved),) + (pix_extent, pix_extent)),
+                    owcs=twcs,
+                )
+                resid = enmap.reproject.thumbnails(
+                    resid,
+                    coords=cent,
+                    oshape=((len(resid),) + (pix_extent, pix_extent)),
+                    owcs=twcs,
+                )
+                resid = resid / fit["aman"].amp.value
+                resid_weights = enmap.reproject.thumbnails_ivar(
+                    resid_weights,
+                    coords=cent,
+                    oshape=((len(redid),) + (pix_extent, pix_extent)),
+                    owcs=twcs,
+                )
+
+                # Add
+                mcoadd.insert(solved * weights, op=op)
+                wcoadd.insert(weights, op=op)
+                rmcoadd.insert(resid * resid_weights, op=op)
+                rwcoadd.insert(resid_weights, op=op)
+
+            # Divide weights
+            with np.errstate(divide="ignore", invalid="ignore"):
+                mcoadd /= wcoadd
+                rmcoadd /= rwcoadd
+            np.nan_to_num(mcoadd, copy=False, nan=0, posinf=0, neginf=0)
+            np.nan_to_num(rmcoadd, copy=False, nan=0, posinf=0, neginf=0)
+
+            # Save and plot
+            for omap, name in [
+                (mcoadd, "stack"),
+                (wcoadd, "stack_weights"),
+                (rmcoadd, "resid_stack"),
+                (rwcoadd, "resid_stack_weights"),
+            ]:
+                enmap.write_map(
+                    os.path.join(
+                        data_dir_spl, "{spl}_{epoch[0]}_{epoch[1]}_{name}.fits"
+                    ),
+                    omap,
+                    "fits",
+                    allow_modify=True,
+                )
+                for comp in ("T", "Q", "U"):
+                    for log in [False, True]:
+                        plot_map(
+                            omap,
+                            pixsize,
+                            extent,
+                            plt_extent,
+                            twcs.wcs.crpix.astype(int),
+                            [0, 0],
+                            1.0,
+                            plot_dir_spl,
+                            spl,
+                            epoch[0],
+                            epoch[1],
+                            comp,
+                            log,
+                            log_thresh,
+                            name,
+                        )
