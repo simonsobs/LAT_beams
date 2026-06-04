ff695d95ac228b6d9f574038be488c0da1e986c9
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2026-06-04 09:22:42 -0700

    feat: much better plotting and some error handing

--- fit_stacked_map.py
+++ fit_stacked_map.py
@@ -1,4 +1,5 @@
 import os
+from typing import cast
 from glob import glob
 
 import astropy.units as u
@@ -14,7 +15,7 @@ from sotodlib.core import AxisManager
 import lat_beams.fitting.models as bm
 from lat_beams.beam_utils import get_fwhm_radial_bins, radial_profile
 from lat_beams.fitting.map import fit_bessel_map, fit_gauss_map, make_guess
-from lat_beams.plotting import plot_map_complete
+from lat_beams.plotting import plot_map_complete, auto_relplot
 from lat_beams.utils import get_args_cfg, setup_cfg, setup_paths
 
 
@@ -75,6 +76,7 @@ if cfg.det_split_dir != "":
 # Loop through splits
 # TODO: Have make_stacked_map save paths in jobdb
 for split in cfg.split_by:
+    i = 0
     print(f"Splitting by {split}")
     data_dir_spl = os.path.join(data_dir, "stacks", split)
     if not os.path.isdir(data_dir_spl):
@@ -84,20 +86,26 @@ for split in cfg.split_by:
         print("Can't fit a non band split!")
         continue
     band_idx = np.where(np.array(split.split("+")) == "band")[0][0]
+    to_plot_r = {s: [] for s in split.split("+")}
+    to_plot_r["r"] = []
+    to_plot_r["rprof"] = []
+    to_plot_r["epoch"] = []
+    to_plot_r["dataset"] = []
+    to_plot_l = {s: [] for s in split.split("+")}
+    to_plot_l["ell"] = []
+    to_plot_l["window"] = []
+    to_plot_l["epoch"] = []
+    to_plot_l["dataset"] = []
+    prof_plot_dir = os.path.join(plot_dir, "stack_profiles", split)
+    os.makedirs(prof_plot_dir, exist_ok=True)
     for spl_dir in sorted([f.path for f in os.scandir(data_dir_spl) if f.is_dir()]):
         spl_rel = os.path.relpath(spl_dir, data_dir_spl)
+        if "mars" in spl_rel: continue
         plot_dir_spl = os.path.join(plot_dir, "stacks", split, spl_rel)
-        prof_plot_dir = os.path.join(plot_dir, "stack_profiles", split, spl_rel)
         os.makedirs(plot_dir_spl, exist_ok=True)
-        os.makedirs(prof_plot_dir, exist_ok=True)
         band = spl_rel.split("+")[band_idx]
         fscale_fac = 90.0 / float(band[1:])
         band_mask_size = np.deg2rad(fscale_fac * cfg.mask_size)
-        labels = []
-        rprofiles = []
-        windows = []
-        mprofiles = []
-        mwindows = []
         for epoch in cfg.epochs:
             plot_dir_epc = os.path.join(plot_dir_spl, f"{epoch[0]}_{epoch[1]}")
             os.makedirs(plot_dir_epc, exist_ok=True)
@@ -117,6 +125,8 @@ for split in cfg.split_by:
                 imap = enmap.read_map(map_path)[0]  # Just T for now
                 ivar = enmap.read_map(ivar_path)[0]  # Just T for now
                 imap = enmap.unapply_window(imap, order=0)
+                imap = cast(enmap.ndmap, imap)
+                ivar = cast(enmap.ndmap, ivar)
                 posmap = imap.posmap()
 
                 # Setup aman for output
@@ -145,6 +155,9 @@ for split in cfg.split_by:
                 if gauss_params is None or model is None:
                     print("\t\tGauss fit failed")
                     continue
+                if abs(gauss_params.amp.value - 1) >= .2:
+                    print("\t\tGauss fit looks bad!")
+                    continue
                 aman.wrap("gauss", gauss_params)
                 for to_parent in ["amp", "off", "xi0", "eta0"]:
                     aman.wrap(to_parent, gauss_params[to_parent])
@@ -161,6 +174,9 @@ for split in cfg.split_by:
                     get_fwhm_radial_bins(r[rmsk], rprof[rmsk], interpolate=True)
                     * u.arcsec
                 )
+                if np.isnan(data_fwhm):
+                    print("\t\tData FWHM is bad! Skipping!")
+                    continue
                 aman.wrap("data_fwhm", data_fwhm)
                 aman.wrap("r", r * u.arcsec)
                 aman.wrap("rprof", rprof * u.pW)
@@ -198,7 +214,7 @@ for split in cfg.split_by:
                     posmap_highres.shape,
                 )
                 mprof = radial_profile(model_highres, cent[::-1])
-                mprof[0] = 1.0
+                mprof /= mprof[0]
                 mr = np.linspace(0, len(mprof), len(mprof)) * pixsize
                 interp = PchipInterpolator(mr, mprof)
                 mr_highres = np.arange(0, cfg.extent_highres, cfg.pixsize_highres)
@@ -244,11 +260,19 @@ for split in cfg.split_by:
                 )
 
                 # Save for plots
-                labels += [f"{epoch[0]}_{epoch[1]}{dstr}"]
-                rprofiles += [rprofile]
-                mprofiles += [mprofile]
-                windows += [window]
-                mwindows += [mwindow]
+                mmsk = mr_highres < 1.2*3600 * np.rad2deg(band_mask_size)
+                dmsk = r < 3600 * np.rad2deg(band_mask_size)
+                to_plot_r["r"] += mr_highres[mmsk].tolist() + r[dmsk].tolist()
+                to_plot_r["rprof"] += mprofile[mmsk, 1].tolist() + rprofile[dmsk, 1].tolist()
+                to_plot_r["epoch"] += [f"{epoch[0]}_{epoch[1]}{dstr}"]*(np.sum(mmsk) + np.sum(dmsk))
+                to_plot_r["dataset"] += ["model"]*np.sum(mmsk) + ["data"]*np.sum(dmsk)
+                to_plot_l["ell"] += ells.tolist() + ells.tolist()
+                to_plot_l["window"] += mwindow[:, 1].tolist() + window[:, 1].tolist()
+                to_plot_l["epoch"] += [f"{epoch[0]}_{epoch[1]}{dstr}"]*(2*len(ells))
+                to_plot_l["dataset"] += ["model"]*len(ells) + ["data"]*len(ells)
+                for sc, si in zip(split.split("+"), spl_rel.split("+")):
+                    to_plot_r[sc] += [si] * (np.sum(mmsk) + np.sum(dmsk))
+                    to_plot_l[sc] += [si] * (2*len(ells))
 
                 # Save and plot maps
                 aman.save(
@@ -287,76 +311,45 @@ for split in cfg.split_by:
                         lognorm=1,
                     )
 
-        # Plot profiles and windows
-        plt.close()
-        for label, rprofile, mprofile in zip(labels, rprofiles, mprofiles):
-            label = label.replace("_", " ")
-            plt.plot(
-                rprofile[:, 0],
-                rprofile[:, 1],
-                label=label + " Data",
-                marker="x",
-                alpha=0.4,
-            )
-            plt.plot(
-                mprofile[:, 0],
-                mprofile[:, 1],
-                label=label + " Model",
-                linestyle="--",
-                color=plt.gca().lines[-1].get_color(),
-                alpha=0.4,
-            )
-        plt.xlim(0, 3600 * np.rad2deg(band_mask_size))
-        plt.ylim((1e-5, 1))
-        plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
-        plt.title(f"{spl_rel} Profile")
-        plt.xlabel('r (")')
-        plt.ylabel("Profile")
-        plt.savefig(
-            os.path.join(prof_plot_dir, f"profile_{spl_rel}.png"), bbox_inches="tight"
-        )
+    # Plot profiles and windows
+    plt.close()
+    plot = auto_relplot(
+        to_plot_r,
+        x="r",
+        y="rprof",
+        kind="line",
+        estimator=None,
+        style="dataset",
+        hue="epoch",
+        merge=([["band", "tube_slot"]] if ("tube_slot" in split and "band" in split) else []),
+        facet_kws={'sharey': True, 'sharex': False},
+    )
+    plot.set_axis_labels('r (")', r"Beam Profile")
+    plot.set(yscale="log")
+    plt.suptitle(f"Beam Profile by {split}")
+    plt.subplots_adjust(top=0.85)
+    plot.figure.tight_layout()
+    plt.savefig(
+        os.path.join(prof_plot_dir, f"profile_{split}.png"), bbox_inches="tight"
+    )
 
-        plt.yscale("log")
-        plt.title(f"{spl_rel} Log Profile")
-        plt.xlabel('r (")')
-        plt.ylabel("Log Profile")
-        plt.savefig(
-            os.path.join(prof_plot_dir, f"profile_{spl_rel}_log.png"),
-            bbox_inches="tight",
-        )
-        plt.close()
-
-        for label, window in zip(labels, windows):
-            label = label.replace("_", " ")
-            plt.loglog(
-                window[:, 0],
-                window[:, 1],
-                label=label,
-                alpha=0.5,
-            )
-        plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
-        plt.title(f"{spl_rel} Window Function")
-        plt.xlabel("l")
-        plt.ylabel("b_l")
-        plt.savefig(
-            os.path.join(prof_plot_dir, f"window_{spl_rel}.png"), bbox_inches="tight"
-        )
-        plt.close()
-
-        for label, window in zip(labels, mwindows):
-            label = label.replace("_", " ")
-            plt.loglog(
-                window[:, 0],
-                window[:, 1],
-                label=label,
-                alpha=0.5,
-            )
-        plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
-        plt.title(f"{spl_rel} Model Window Function")
-        plt.xlabel("l")
-        plt.ylabel("b_l")
-        plt.savefig(
-            os.path.join(prof_plot_dir, f"model_window_{spl_rel}.png"),
-            bbox_inches="tight",
-        )
-        plt.close()
+    plt.close()
+    plot = auto_relplot(
+        to_plot_l,
+        x="ell",
+        y="window",
+        kind="line",
+        estimator=None,
+        style="dataset",
+        hue="epoch",
+        merge=([["band", "tube_slot"]] if ("tube_slot" in split and "band" in split) else []),
+        facet_kws={'sharey': False, 'sharex': True},
+    )
+    plot.set_axis_labels(r"$\ell$", r"Beam Window Function ($B_{\ell}^{T}$)")
+    plot.set(ylim=(0, None))
+    plt.suptitle(f"Beam Profile by {split}")
+    plt.subplots_adjust(top=0.85)
+    plot.figure.tight_layout()
+    plt.savefig(
+        os.path.join(prof_plot_dir, f"window_{split}.png"), bbox_inches="tight"
+    )
