f3a448b67ae2405445bafe9a5ce6ffbe831c0521
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2026-06-23 09:53:51 -0700

    slm: switch to preemt for tod fits

---
+++ preempt-fit-point.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+preempt_handler()
+{
+    #place here: commands to run when preempt signal (SIGTERM) arrives from slurm
+    kill -TERM ${1} #forward SIGTERM signal to the user application
+    #if --requeue was used, slurm will automatically do so here
+}
+timeout_handler()
+{
+    #place here: commands to run when timeout signal (set outside to USR1) arrives
+    kill -TERM ${1} #forward SIGTERM signal to the user application
+    #manually requeue here because slurm *will not* do it automatically
+    scontrol requeue ${SLURM_JOB_ID}
+}
+
+python fit_pointing.py configs/fit_pointing/nersc.yaml -f 16 &
+pid=$!
+trap "preempt_handler '$pid'" SIGTERM #this catches preempt SIGTERM from slurm
+trap "timeout_handler '$pid'" USR1 #this catches timeout USR1 from slurm
+wait
+sleep 120 #keep the job step alive until slurm sends SIGKILL
